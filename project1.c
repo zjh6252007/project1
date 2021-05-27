5d708a8f2a7801d7c1e7beb5b8faf4663f19b2d2
TangyrenMidKing <65498450+TangyrenMidKing@users.noreply.github.com>
2021-05-27 09:34:30 -0400

    Add files via upload

--- project1.c
+++ project1.c
@@ -51,19 +51,62 @@ int main(void)
         pas[sp] = ir.m;
         break;
       // 02 – OPR 0, M Operation to be performed on the data at the top of the stack.
+      case 2:
+        break;
       // 03 – LOD L, M Load value to top of stack from the stack location at
       // set M from L lexicographical levels down
+      case 3:
+        sp++;
+        pas[sp] = pas[base(ir.l) + ir.m];
+        break;
       // 04 – STO L, M Store value at top of stack in the stack location at offset M
       // from L lexicographical levels down
+      case 4:
+        pas[base(ir.l) + ir.m] = pas[sp];
+        sp--;
+        break;
       // 05 – CAL L, M Call procedure at code index M (generates new
       // Activation Record and PC <- M)
+      case 5:
+        pas[sp + 1] = base(ir.l); // static link (SL)
+        pas[sp + 2] = bp; // dynamic link (DL)
+        pas[sp + 3] = pc; // return address (RA)
+        bp = sp + 1;
+        pc = ir.m;
+        break;
       // 06 – INC 0, M Allocate M memory words (increment SP by M). First four
       // are reserved to Static Link (SL), Dynamic Link (DL), Return Address (RA), and Parameter (P)
-      // 07 – JMP 0, M Jump to instruction M (PC  M)
+      case 6:
+        sp += ir.m;
+        break;
+      // 07 – JMP 0, M Jump to instruction M (PC <- M)
+      case 7:
+        pc = ir.m;
+        break;
       // 08 – JPC 0, M Jump to instruction M if top stack element is 0
+      case 8:
+        if (pas[sp] == 0)
+          pc = ir.m;
+        sp--;
+        break;
       // 09 – SYS 0, 1 Write the top stack element to the screen
       //      SYS 0, 2 Read in input from the user and store it on top of the stack
       //      SYS 0, 3 End of program (Set Halt flag to zero)
+      case 9:
+        if (ir.m == 1)
+        {
+          printf("Output result is: %d\n", pas[sp]);
+          sp--;
+        }
+        else if (ir.m == 2)
+        {
+          sp++;
+          printf("Please Enter an Integer: ");
+          scanf("%d", &pas[sp]);
+        }
+        else
+          halt = 0;
+        break;
       dafalut:
         printf("Wrong op code!\n");
         return 0;
@@ -73,15 +116,15 @@ int main(void)
   return 0;
 }
 
-// int base(int L)
-// {
-//   int arb = BP; // arb = activation record base
-//
-//   while ( L > 0) //find base L levels down
-//   {
-//     arb = pas[base];
-//     L--;
-//   }
-//
-//   return arb;
-// }
+int base(int L)
+{
+  int arb = bp; // arb = activation record base
+
+  while (L > 0) //find base L levels down
+  {
+    arb = pas[base];
+    L--;
+  }
+
+  return arb;
+}
